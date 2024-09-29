63d2e26162e06f8f3980eaa0e0b5faf77c93b9f6
Archie <archiesua@gmail.com>
2024-09-29 20:15:23 +1300

    using the findElement - cssSelector method to find the error message when a wrong password is signin, also printing out the error message to the console and using the implicitWait command to direct the webdriver to wait for 5 seconds until the error shows up.

--- src/Locators.java
+++ src/Locators.java
@@ -1,8 +1,24 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Locators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Archie\\Documents\\chrome-win64\\chrome-win64\\chrome.exe");
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver chromeDriver = new ChromeDriver();
+		chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		chromeDriver.get("https://rahulshettyacademy.com/locatorspractice/");
+		chromeDriver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		chromeDriver.findElement(By.name("inputPassword")).sendKeys("password");
+		chromeDriver.findElement(By.className("signInBtn")).click();
+		System.out.println(chromeDriver.findElement(By.cssSelector("p.error")).getText());
+		
+		//chromeDriver.close();
 
 	}
 
